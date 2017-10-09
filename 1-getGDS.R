0966baa7b4e240bdd09c170097994c7d3e50fdd0
Francisco Lima <francisco.lima278@gmail.com>
2017-10-09 11:27:35 +0100

    Update 1-getGDS.R

--- 1-getGDS.R
+++ 1-getGDS.R
@@ -2,7 +2,7 @@
 # Merge all files and write as PLINK again; we need it in order to correct for kinship
 
 library(snpStats)
-conversionTable <- read.delim("conversionTable.txt", sep = "\t", stringsAsFactors = F)
+load("conversionTable.RData")
 
 pathM <- paste("Genomics/108Malay_2527458snps", c(".bed", ".bim", ".fam"), sep = "")
 SNP_M <- read.plink(pathM[1], pathM[2], pathM[3])
@@ -19,8 +19,8 @@ colnames(map) <- c("chr", "SNP", "gen.dist", "position", "A1", "A2")
 SNP_M$fam<- rbind(SNP_M$fam, SNP_I$fam, SNP_C$fam)
 
 # Rename SNPs present in the conversion table into rs IDs
-mappedSNPs <- intersect(SNP_M$map$SNP, conversionTable$Name)
-newIDs <- conversionTable$RsID[match(SNP_M$map$SNP[SNP_M$map$SNP %in% mappedSNPs], conversionTable$Name)]
+mappedSNPs <- intersect(SNP_M$map$SNP, names(conversionTable))
+newIDs <- conversionTable[match(SNP_M$map$SNP[SNP_M$map$SNP %in% mappedSNPs], names(conversionTable))]
 SNP_M$map$SNP[rownames(SNP_M$map) %in% mappedSNPs] <- newIDs
 
 write.plink("convertGDS", snps = SNP_M$genotypes)
